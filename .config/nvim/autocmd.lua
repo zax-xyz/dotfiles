5bfeec729ce2bef84ebe2cc7d2c8d111278fb13d
Michael Vo <zax@zaxu.xyz>
2022-08-20 22:10:06 +1000

    feat(nvim): expect callback in ftAutocmd

--- .config/nvim/autocmd.lua
+++ .config/nvim/autocmd.lua
@@ -1,53 +1,39 @@
-local function ftAutocmd(filetype, opts)
-  opts.pattern = filetype
-  vim.api.nvim_create_autocmd('FileType', opts)
+local function ftAutocmd(filetype, callback)
+  vim.api.nvim_create_autocmd('FileType', {
+    pattern = filetype,
+    callback = callback,
+  })
 end
 
-ftAutocmd('python', {command = 'hi link pythonNone Structure'})
+ftAutocmd('python', function()
+  vim.cmd('hi link pythonNone Structure')
+end)
 
 -- Indentation
-ftAutocmd('python,c,cpp,go,java', {
-  callback = function()
-    local options = {'tabstop', 'softtabstop', 'shiftwidth'}
-    for i = 1, #options do
-      vim.opt_local[options[i]] = 4
-    end
-  end,
-})
-ftAutocmd('go', {
-  callback = function()
-    vim.opt_local.expandtab = false
-  end,
-})
+ftAutocmd('python,c,cpp,go,java', function()
+  local options = {'tabstop', 'softtabstop', 'shiftwidth'}
+  for i = 1, #options do
+    vim.opt_local[options[i]] = 4
+  end
+end)
+ftAutocmd('go', function()
+  vim.opt_local.expandtab = false
+end)
 
-ftAutocmd('plaintex', {
-  callback = function()
-    vim.opt_local.filetype = 'tex'
-  end,
-})
-ftAutocmd('rmd', {
-  callback = function()
-    vim.opt_local.filetype = 'markdown'
-  end,
-})
-ftAutocmd('r', {
-  callback = function()
-    vim.keymap.set('i', '<A-->', '<-', {noremap = true, buffer = true})
-  end,
-})
-ftAutocmd('arduino', {
-  callback = function()
-    vim.opt_local.filetype = 'cpp'
-    vim.opt_local.syntax = 'arduino'
-  end,
-})
+ftAutocmd('plaintex', function()
+  vim.opt_local.filetype = 'tex'
+end)
+ftAutocmd('rmd', function()
+  vim.opt_local.filetype = 'markdown'
+end)
+ftAutocmd('r', function()
+  vim.keymap.set('i', '<A-->', '<-', {noremap = true, buffer = true})
+end)
+ftAutocmd('arduino', function()
+  vim.opt_local.filetype = 'cpp'
+  vim.opt_local.syntax = 'arduino'
+end)
 
-ftAutocmd('tex,markdown,nroff', {
-  callback = function()
-    vim.opt_local.spell = true
-  end,
-})
-
-ftAutocmd('c,cpp', {
-  command = [[syn match cType "\<[a-zA-Z_][a-zA-Z0-9_]*_[t]\>"]],
-})
+ftAutocmd('tex,markdown,nroff', function()
+  vim.opt_local.spell = true
+end)
