354612aa62b2e07c42f41b1ee3bd9150b7fa2c69
Michael Vo <zax@zaxu.xyz>
2022-09-21 18:32:30 +1000

    refactor(nvim): move transparency mapping from mappings to onedark config

--- .config/nvim/mappings.lua
+++ .config/nvim/mappings.lua
@@ -90,12 +90,3 @@ end
 -- copy to system clipboard
 bind("", "<leader>y", '"+y')
 bind("", "<leader>Y", ':%y+<CR>')
-
-local transparent = true
-bind("", "<leader>t", function()
-    transparent = not transparent
-    require('onedark').setup {
-        transparent = transparent
-    }
-    require('onedark').load()
-end)
--- .config/nvim/plugins/onedark.lua
+++ .config/nvim/plugins/onedark.lua
@@ -1,4 +1,15 @@
+local bind = require('util.bind')
+
 require('onedark').setup {
     transparent = vim.fn.exists('g:neovide') == 0,
 }
 require('onedark').load()
+
+local transparent = true
+bind("", "<leader>t", function()
+    transparent = not transparent
+    require('onedark').setup {
+        transparent = transparent
+    }
+    require('onedark').load()
+end)
