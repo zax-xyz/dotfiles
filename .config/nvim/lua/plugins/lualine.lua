1c76a21c3fd7b1e6a50d85f67e63f5f33c0dd5e8
Michael Vo <zax@zaxu.xyz>
2022-11-19 02:19:32 +1100

    feat(nvim): round lualine style

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -2,20 +2,24 @@ require('lualine').setup {
     options = {
         icons_enabled = true,
         theme = 'auto',
-        component_separators = { left = '', right = ''},
-        section_separators = { left = '', right = ''},
+        component_separators = '·',
+        section_separators = { left = '', right = ''},
         disabled_filetypes = {},
         always_divide_middle = true,
         globalstatus = false,
     },
     sections = {
-        lualine_a = {'mode'},
-        lualine_b = {'branch', 'diff', 'diagnostics'},
-        lualine_c = {'filename'},
+        lualine_a = {
+            { function() return '' end, separator = { left = '' }, }
+        },
+        lualine_b = {'branch'},
+        lualine_c = {'filename', 'diff', 'diagnostics'},
         -- lualine_x = {'encoding', 'fileformat', 'filetype'},
-        lualine_x = {'encoding', 'filetype'},
+        lualine_x = {'filetype'},
         lualine_y = {'progress'},
-        lualine_z = {'location'}
+        lualine_z = {
+            { 'location', separator = { right = '' } },
+        },
     },
     inactive_sections = {
         lualine_a = {},
