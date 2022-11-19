34a8bf976c63807dbf0e213cd2a0866eb61fb33a
Michael Vo <zax@zaxu.xyz>
2022-11-19 23:25:44 +1100

    feat(neovide): background colour matching colour scheme
    this doesn't seem to be working tho lol

--- .config/nvim/lua/neovide.lua
+++ .config/nvim/lua/neovide.lua
@@ -5,7 +5,7 @@ vim.g.gui_font_size = vim.g.gui_font_default_size
 vim.g.gui_font_face = 'Iosevka Term Extended'
 
 vim.g.neovide_transparency = 0.95
-vim.g.neovide_background_color = '#282c34'
+vim.g.neovide_background_color = '#1e1e2e'
 
 local refreshGuiFont = function()
   vim.opt.guifont = string.format("%s:h%s",vim.g.gui_font_face, vim.g.gui_font_size)
