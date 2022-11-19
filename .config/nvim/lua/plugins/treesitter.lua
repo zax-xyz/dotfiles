480a358533d259fa2a77800164f28652949b0263
Michael Vo <zax@zaxu.xyz>
2022-11-20 00:34:46 +1100

    fix(nvim): disable treesitter indent for python

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,10 @@ require'nvim-treesitter.configs'.setup {
     -- Modules and its options go here
     highlight = { enable = true },
     incremental_selection = { enable = true },
-    indent = { enable = true },
+    indent = {
+        enable = true,
+        disable = {"python"}
+    },
     textobjects = { enable = true },
     matchup = { enable = true },
 }
