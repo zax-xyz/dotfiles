1f74964eae82698f5f7ba0e08efa3b11a90f4ce2
Michael Vo <zax@zaxu.xyz>
2022-11-20 14:37:59 +1100

    refactor(nvim): remove settings unnecessary with neovim

--- .config/nvim/lua/basic.lua
+++ .config/nvim/lua/basic.lua
@@ -1,9 +1,6 @@
 local bind = require('util.bind')
 
--- Basics
 vim.g.mapleader = ' '
-vim.opt.encoding = 'utf-8'
-vim.opt.compatible = false
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
@@ -12,27 +9,11 @@ vim.opt.cursorline = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
--- Highlight search while typing
-vim.opt.incsearch = true
-vim.opt.hlsearch = true
-
--- Tell me what command I'm typing
-vim.opt.showcmd = true
-
 -- Make command mode tab complete nicer
 vim.opt.wildmode = 'longest,full'
-vim.opt.wildmenu = true
 
 -- Mappings timeout
 vim.opt.timeoutlen = 350
-
--- Make vim faster
-vim.opt.ttyfast = true
-
--- Mode cursors
-vim.o.t_SI = [[\<Esc>[5 q]]
-vim.o.t_SR = [[\<Esc>[3 q]]
-vim.o.t_EI = [[\<Esc>[1 q]]
 
 -- Enable folding
 vim.opt.foldmethod = 'expr'
@@ -43,9 +24,6 @@ bind('n', '<leader>F', 'za')
 -- Open splits at the bottom and right
 vim.opt.splitbelow = true
 vim.opt.splitright = true
-
--- Allow opening a new tab when current buffer has unsaved changes
-vim.opt.hidden = true
 
 -- Toggle paste mode with F2
 vim.opt.pastetoggle = '<F2>'
@@ -69,9 +47,6 @@ local options = {'tabstop', 'softtabstop', 'shiftwidth'}
 for i = 1, #options do
     vim.opt[options[i]] = 4
 end
-
--- Color scheme
-vim.opt.background = 'dark'
 
 vim.opt.re = 0
 vim.opt.scrolloff = 3
