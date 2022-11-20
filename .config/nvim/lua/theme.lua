ffb0cba02453ad3a5a631872182b1db2d1fc6430
Michael Vo <zax@zaxu.xyz>
2022-11-20 11:29:51 +1100

    feat: use opaque terminal+vim background

--- .config/nvim/lua/theme.lua
+++ .config/nvim/lua/theme.lua
@@ -1,7 +1,7 @@
 vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
 
 require("catppuccin").setup({
-    transparent_background = vim.fn.exists('g:neovide') == 0,
+    -- transparent_background = vim.fn.exists('g:neovide') == 0,
 })
 
 vim.cmd [[colorscheme catppuccin]]
