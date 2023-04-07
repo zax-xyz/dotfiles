7d16bcc7b04aae58e437c4f007c34d0ae8a67949
Michael Vo <zax@zaxu.xyz>
2023-04-07 19:55:05 +1000

    feat(polybar): launch on all monitors
    imagine spawning 3n different polybar instances 💀

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,10 +6,19 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar -r left &
-polybar -r centre &
-polybar -r right &
-# polybar -r full &
+launch() {
+    polybar -r left &
+    polybar -r centre &
+    polybar -r right &
+    # polybar -r full &
+}
+
+if type xrandr; then
+    for m in $(xrandr | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m launch
+    done
+else
+    launch
+fi
 
 echo "Bars launched..."
