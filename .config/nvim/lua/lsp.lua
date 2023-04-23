d0dfd89985f0fc90ddb67e1e001a09e5a6642916
Michael Vo <zax@zaxu.xyz>
2023-04-23 18:47:27 +1000

    feat(nvim): eslint formatting

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -225,7 +225,7 @@ null_ls.setup({
         null_ls.builtins.diagnostics.eslint_d,
         null_ls.builtins.diagnostics.shellcheck,
         null_ls.builtins.formatting.prettier,
-        -- null_ls.builtins.formatting.eslint_d,
+        null_ls.builtins.formatting.eslint_d,
         null_ls.builtins.formatting.rustfmt,
         null_ls.builtins.formatting.gofmt,
         -- null_ls.builtins.formatting.lua_format,
