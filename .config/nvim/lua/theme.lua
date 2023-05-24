4d93b94879e2bf1161b9f23741b626055566c949
Michael Vo <zax@zaxu.xyz>
2023-05-25 03:32:34 +1000

    feat(nvim): use catppuccin term colors

--- .config/nvim/lua/theme.lua
+++ .config/nvim/lua/theme.lua
@@ -2,6 +2,7 @@ vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
 
 require("catppuccin").setup({
     -- transparent_background = vim.fn.exists('g:neovide') == 0,
+    term_colors = true,
 })
 
 vim.cmd [[colorscheme catppuccin]]
