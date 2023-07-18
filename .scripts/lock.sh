a1b16c7fa232c2741c9b95b124cbd1f98d52a33d
Michael Vo <zax@zaxu.xyz>
2023-07-19 01:08:50 +1000

    feat(lock): position time in corner, add last unlock time

--- .scripts/lock.sh
+++ .scripts/lock.sh
@@ -1,44 +1,33 @@
 #!/bin/sh
 
 base='#1e1e2e'
-mantle='#181825'
-crust='#11111b'
-
 text='#cdd6f4'
-subtext0='#a6adc8'
-subtext1='#bac2de'
-
-surface0='#313244'
-surface1='#45475a'
-surface2='#585b70'
-
-overlay0='#6c7086'
 overlay1='#7f849c'
-overlay2='#9399b2'
 
-blue='#89b4fa'
 lavender='#b4befe'
-sapphire='#74c7ec'
-sky='#89dceb'
-teal='#94e2d5'
-green='#a6e3a1'
-yellow='#f9e2af'
-peach='#fab387'
-maroon='#eba0ac'
 red='#f38ba8'
-mauve='#cba6f7'
-pink='#f5c2e7'
-flamingo='#f2cdcd'
-rosewater='#f5e0dc'
 
 B='#00000000'  # blank
 F='Lato Light' # Font
+
+time_size=72
+date_size=28
+greeter_size=28
+
+mx=128
+my=128
+
+last_unlock_path="$HOME/.local/share/last_unlock"
+
+if [ -f "$last_unlock_path" ]; then
+    greeting="Last unlock $(date -r "$last_unlock_path" "+%a %d %b %T")"
+fi
 
 killall -SIGUSR1 dunst
 # mpc pause
 [ -z "$(pidof i3lock)" ] && i3lock  \
     --color=$base \
-    --image=$HOME/Pictures/wallpapers/2_1080.png \
+    --image="$HOME/Pictures/wallpapers/canvas_1080.png" \
     -F \
     --insidever-color=${base}18 \
     --ringver-color=$lavender \
@@ -61,14 +50,22 @@ killall -SIGUSR1 dunst
     --keyhl-color=$lavender \
     --bshl-color=$red \
     \
-    --clock \
+    --force-clock \
+    --time-pos="$mx:$my + $time_size" \
+    --date-pos="tx+5:ty + $time_size/2 + 8" \
+    --greeter-pos="tx:h-$my" \
     --time-str="%H:%M:%S" \
     --date-str="%a, %d %b %Y" \
+    --greeter-text="$greeting" \
     --wrong-text="" \
     --noinput-text="" \
     --verif-text="" \
     --nofork \
     -f \
+    \
+    --time-align=1 \
+    --date-align=1 \
+    --greeter-align=1 \
     \
     --time-font="$F" \
     --date-font="$F" \
@@ -76,13 +73,15 @@ killall -SIGUSR1 dunst
     --verif-font="$F" \
     --wrong-font="$F" \
     --greeter-font="$F" \
-    --time-size=36 \
-    --date-size=16 \
-    --radius=94 \
+    --time-size="$time_size" \
+    --date-size="$date_size" \
+    --greeter-size="$greeter_size" \
+    --radius=92 \
     --ring-width=5 \
     --pass-screen-keys \
     --pass-media-keys \
     --pass-volume-keys \
     --pass-power-keys
 
+touch "$last_unlock_path"
 killall -SIGUSR2 dunst
