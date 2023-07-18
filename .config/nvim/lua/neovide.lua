8ec782dde01cbb7c074c2b047f2931fe7e34236e
Michael Vo <zax@zaxu.xyz>
2023-07-19 01:03:19 +1000

    feat: enable blur

--- .config/nvim/lua/neovide.lua
+++ .config/nvim/lua/neovide.lua
@@ -4,7 +4,7 @@ vim.g.gui_font_default_size = 10
 vim.g.gui_font_size = vim.g.gui_font_default_size
 vim.g.gui_font_face = 'Iosevka Term'
 
-vim.g.neovide_transparency = 0.95
+vim.g.neovide_transparency = 0.9
 vim.g.neovide_background_color = '#1e1e2e'
 
 local refreshGuiFont = function()
