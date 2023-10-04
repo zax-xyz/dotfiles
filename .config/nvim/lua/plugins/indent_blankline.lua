1aff2da4ddbe5829ba47924792a8e55c7bbf4ebd
Michael Vo <zax@zaxu.xyz>
2023-10-04 13:53:50 +1100

    feat(nvim/ibl): don't show end

--- .config/nvim/lua/plugins/indent_blankline.lua
+++ .config/nvim/lua/plugins/indent_blankline.lua
@@ -5,6 +5,7 @@ vim.api.nvim_set_hl(0, 'IblScope', { bg = 'NONE', fg = colors.lavender })
 
 require("ibl").setup({
     scope = {
-        show_start = false
+        show_start = false,
+        show_end = false,
     }
 })
