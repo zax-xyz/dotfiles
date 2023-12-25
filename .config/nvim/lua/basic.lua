670cc12fa68c66e263ef889bb974be52f3f611c5
Michael Vo <zax@zaxu.xyz>
2023-12-25 13:01:59 +1100

    refactor(nvim): remove tmux settings

--- .config/nvim/lua/basic.lua
+++ .config/nvim/lua/basic.lua
@@ -37,10 +37,6 @@ if vim.fn.has("termguicolors") == 1 then
     vim.opt.termguicolors = true
 end
 
--- Fix colors in tmux
-vim.o.t_8f = [[\<Esc>[38;2;%lu;%lu;%lum]]
-vim.o.t_8b = [[\<Esc>[48;2;%lu;%lu;%lum]]
-
 -- Indentation rules
 vim.opt.expandtab = true
 local options = { 'tabstop', 'softtabstop', 'shiftwidth' }
