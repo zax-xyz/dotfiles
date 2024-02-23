c86102d103c8b4f15880b3491c6c63f37ed4d1da
Michael Vo <zax@zaxu.xyz>
2024-02-23 17:17:35 +1100

    feat: git hunk navigation

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,6 +1,41 @@
-require('gitsigns').setup {
+local gs = require("gitsigns")
+gs.setup {
     current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
     current_line_blame_opts = {
         delay = 100,
     },
 }
+
+local wk = require("which-key")
+
+wk.register(
+    {
+        ["]c"] = {
+            function()
+                if vim.wo.diff then
+                    return ']c'
+                end
+                vim.schedule(function()
+                    gs.next_hunk()
+                end)
+                return '<Ignore>'
+            end,
+            "Jump to next hunk",
+        },
+        ["[c"] = {
+            function()
+                if vim.wo.diff then
+                    return '[c'
+                end
+                vim.schedule(function()
+                    gs.prev_hunk()
+                end)
+                return '<Ignore>'
+            end,
+            "Jump to previous hunk",
+        },
+    },
+    {
+        mode = { "n", "v" }
+    }
+)
