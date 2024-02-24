bfbb14d175e57e61c63153777b232fd280c664f3
Michael Vo <zax@zaxu.xyz>
2024-02-24 19:57:10 +1100

    feat(nvim): disable kitty workaround
    cringe

--- .config/nvim/lua/theme.lua
+++ .config/nvim/lua/theme.lua
@@ -3,6 +3,7 @@ vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
 require("catppuccin").setup({
     -- transparent_background = vim.fn.exists('g:neovide') == 0,
     term_colors = true,
+    kitty = false,
 })
 
 vim.cmd [[colorscheme catppuccin]]
