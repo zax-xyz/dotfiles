5d33ce141a9095b13cbaac3acdf0453f12563522
Michael Vo <zax@zaxu.xyz>
2024-03-02 22:17:02 +1100

    feat(nvim): use lspconfig recommended config for lua_ls

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -123,22 +123,36 @@ local function config(_config)
 end
 
 lspconfig.lua_ls.setup(config({
-    settings = {
-        Lua = {
-            runtime = {
-                version = "LuaJIT",
-                path = vim.split(package.path, ";"),
-            },
-            diagnostics = { globals = { "vim" } },
-            workspace = {
-                library = {
-                    [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                    [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+    on_init = function(client)
+        local path = client.workspace_folders[1].name
+        if not vim.loop.fs_stat(path..'/.luarc.json') and not vim.loop.fs_stat(path..'/.luarc.jsonc') then
+            client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
+                Lua = {
+                    runtime = {
+                        -- Tell the language server which version of Lua you're using
+                        -- (most likely LuaJIT in the case of Neovim)
+                        version = 'LuaJIT'
+                    },
+                    -- Make the server aware of Neovim runtime files
+                    workspace = {
+                        checkThirdParty = false,
+                        library = {
+                            vim.env.VIMRUNTIME,
+                            -- Depending on the usage, you might want to add additional paths here.
+                            -- E.g.: For using `vim.*` functions, add vim.env.VIMRUNTIME/lua.
+                            require("neodev.config").types(),
+                            "${3rd}/luv/library",
+                            "${3rd}/busted/library",
+                        }
+                        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+                        -- library = vim.api.nvim_get_runtime_file("", true)
+                    },
+                    hint = { enable = true },
                 },
-            },
-            hint = { enable = true }
-        }
-    }
+            })
+        end
+        return true
+    end,
 }))
 
 local tsserver_settings = {
