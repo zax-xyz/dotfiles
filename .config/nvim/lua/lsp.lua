d101a81be4097b04b96ca0b291bc5f96e089d886
Michael Vo <zax@zaxu.xyz>
2025-03-05 15:35:23 +1100

    feat(nvim): buf

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -321,6 +321,7 @@ null_ls.setup({
         -- null_ls.builtins.code_actions.eslint_d,
         -- null_ls.builtins.diagnostics.eslint_d,
         null_ls.builtins.diagnostics.shellcheck,
+        null_ls.builtins.diagnostics.buf,
         -- null_ls.builtins.formatting.prettier,
         null_ls.builtins.formatting.prettierd,
         -- null_ls.builtins.formatting.eslint_d,
