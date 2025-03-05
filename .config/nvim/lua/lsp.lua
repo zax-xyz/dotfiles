f7811fa2b1d21c088a9a5320b61b54e8f293ec10
Michael Vo <zax@zaxu.xyz>
2025-03-05 15:33:40 +1100

    feat(nvim): cmp selection bindings

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -253,6 +253,8 @@ cmp.setup({
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
         ['<Tab>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ['<C-j>'] = cmp.mapping.select_next_item(),
+        ['<C-k>'] = cmp.mapping.select_prev_item(),
     }),
     formatting = {
         fields = { "kind", "abbr", "menu" },
